75f2943dc6297f2e4be0de56a99eb3a3e46acef6
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-17 15:53:18 -0500

    buildbot: 4/17 15:53

--- frontend/plugins/toast.js
+++ frontend/plugins/toast.js
@@ -2,4 +2,13 @@ import Vue from 'vue'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
-Vue.use(Toast)
+const options = {
+    toastClassName: "toast",
+    showCloseButtonOnHover: true,
+    closeButtonClassName: "toast-close",
+    hideProgressBar: false,
+    timeout: 2500,
+    maxToasts: 3
+}
+
+Vue.use(Toast, options)

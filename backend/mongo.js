788a756e9c18e6643bc51c552363e9417f1c9dbe
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-18 11:21:40 -0500

    buildbot: 4/18 11:21

--- backend/mongo.js
+++ backend/mongo.js
@@ -4,15 +4,21 @@ dotenv.config()
 
 const MONGO_STRING = process.env.MONGO_STRING
 
+const failureCount = 0
+
 const connectWithRetry = async () => {
     console.log('MongoDB connection with retry')
     return mongoose.connect(MONGO_STRING, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "weasel-backend-dev",retryReads:true, retryWrites:true }).catch((err) => {
         console.log('MongoDB connection unsuccessful, retry after 1 seconds.')
         setTimeout(connectWithRetry, 1000)
+        failureCount++
     })
 }
 
-connectWithRetry()
+if (failureCount < 20){
+    connectWithRetry()
+}
+
 
 const db = mongoose.connection
 
@@ -20,12 +26,16 @@ db.on('error', (error) => {
     console.log(error)
     mongoose.disconnect().then(() => {
         console.log('Database disconnected')
+        if (failureCount < 20){
         connectWithRetry()
+        failureCount++
+        }
     })
 })
 
 db.on('connected', () => {
-    console.log('Database Connected');
+    console.log('Database Connected')
+    failureCount = 0
 })
 
 db.on('disconnected', () => {

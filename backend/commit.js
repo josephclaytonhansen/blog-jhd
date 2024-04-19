93350c6adb0889ed200939cad558edc18f9c4233
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-19 11:02:18 -0500

    buildbot: 4/19 11:02

--- backend/commit.js
+++ backend/commit.js
@@ -2,7 +2,7 @@ import { execSync } from "child_process"
 import { Octokit } from "@octokit/rest"
 import { parse } from "leasot"
 import fs from "fs"
-import { glob } from "glob"
+import "glob"
 
 const date = new Date()
 const formattedDate = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes() < 10 ? "0" : ""}${date.getMinutes()}`
@@ -17,12 +17,10 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 })
 
-console.log(octokit)
-
 const owner = "josephclaytonhansen";
 const repo = "blog-jhd";
 
-glob("**/*.js", (err, files) => {
+glob("**/*.js", { ignore: ["**/node_modules/**", "**/commit.js"] }, (err, files) => {
   if (err) {
     console.error(err);
     return

ca5c033cb8b045b64b7fc893adb89d17326bc141
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-19 11:07:40 -0500

    buildbot: 4/19 11:07

--- backend/commit.js
+++ backend/commit.js
@@ -2,7 +2,7 @@ import { execSync } from "child_process"
 import { Octokit } from "@octokit/rest"
 import { parse } from "leasot"
 import fs from "fs"
-import {glob} from "glob"
+import path from "path"
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
@@ -13,50 +13,53 @@ console.log(`Current working directory: ${process.cwd()}`);
 const owner = "josephclaytonhansen";
 const repo = "blog-jhd";
 
-glob("**/*.js", { ignore: ["**/node_modules/**", "**/commit.js"] }, (err, files) => {
-  console.log(`Files: ${files}`)
-  if (err) {
-    console.error(err);
-    return
+function fromDir(startPath, filter, callback){
+  if (!fs.existsSync(startPath)){
+      console.log("no dir ", startPath);
+      return;
   }
 
-  files.forEach((file) => {
-    console.log(`Checking file: ${file}`)
-    if (file.includes("node_modules")) {
-      return
-    }
-    if (file.includes("commit.js")) {
-      return
-    }
-    fs.readFile(file, "utf8", (err, data) => {
-      if (err) {
-        console.error(err)
-        return
+  const files = fs.readdirSync(startPath);
+  for(let i=0;i<files.length;i++){
+      const filename=path.join(startPath,files[i]);
+      const stat = fs.lstatSync(filename);
+      if (stat.isDirectory()){
+          fromDir(filename,filter,callback); //recurse
       }
+      else if (filename.indexOf(filter)>=0) {
+          fs.readFile(filename, "utf8", (err, data) => {
+              if (err) {
+                  console.error(err)
+                  return
+              }
 
-      const todos = parse(data)
+              const todos = parse(data)
 
-      todos.forEach((todo) => {
-        if (todo.tag === "TODO") {
-          console.log(`Creating issue for TODO: ${todo.text}`)
-          octokit.issues.create({
-            owner,
-            repo,
-            title: `TODO: ${todo.text}`,
-            body: `This issue is automatically created by a build script.\n\nFile: ${todo.file}\nLine: ${todo.line}\nDescription: ${todo.text}`,
-          })
-        }
-      })
-    })
-  })
-})
+              todos.forEach((todo) => {
+                  if (todo.tag === "TODO") {
+                      console.log(`Creating issue for TODO: ${todo.text}`)
+                      octokit.issues.create({
+                          owner,
+                          repo,
+                          title: `TODO: ${todo.text}`,
+                          body: `This issue is automatically created by a build script.\n\nFile: ${todo.file}\nLine: ${todo.line}\nDescription: ${todo.text}`,
+                      })
+                  }
+              })
+          });
+      }
+  }
+}
 
+fromDir(process.cwd(), '.js', function(filename){
+  console.log('-- found: ',filename);
+});
 const date = new Date()
 const formattedDate = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes() < 10 ? "0" : ""}${date.getMinutes()}`
 const commitMessage = `buildbot: ${formattedDate}`
-
 execSync(
   `git add . && git commit --allow-empty -m "${commitMessage}" && git push`,
   { stdio: "inherit" },
 )
+
 

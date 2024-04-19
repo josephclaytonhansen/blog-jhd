761339e5b61bbfdad0dabd070c312ed100ce132a
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-19 11:40:22 -0500

    buildbot: 4/19 11:40

--- frontend/commit.js
+++ frontend/commit.js
@@ -1,10 +1,110 @@
-import { execSync } from "child_process"
+import {
+  execSync
+} from "child_process"
+import {
+  Octokit
+} from "@octokit/rest"
+import fs from "fs"
+import path from "path"
+import dotenv from "dotenv"
+dotenv.config()
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+function parse(filename, data) {
+  const lines = data.split('\n');
+  const todos = lines
+    .map((line, index) => {
+      const match = line.match(/\/\/ TODO: (.*)/)
+      let fil = filename.split('blog-jhd')[1]
+      if (match) {
+        return {
+          tag: 'TODO',
+          text: match[1],
+          line: index + 1,
+          file: fil,
+        };
+      }
+    })
+    .filter(Boolean);
+  return todos;
+}
+
+console.log(`Current working directory: ${process.cwd()}`);
+
+const owner = "josephclaytonhansen";
+const repo = "blog-jhd";
+
+function fromDir(startPath, filter, callback) {
+  if (!fs.existsSync(startPath)) {
+    console.log("no dir ", startPath);
+    return;
+  }
+
+  const files = fs.readdirSync(startPath);
+  for (let i = 0; i < files.length; i++) {
+    const filename = path.join(startPath, files[i]);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      if (filename.includes('/node_modules')) {
+        continue; // Skip node_modules directory
+      }
+      fromDir(filename, filter, callback); //recurse
+    } else if (filename.indexOf(filter) >= 0 && !filename.endsWith('commit.js')) {
+      fs.readFile(filename, "utf8", (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+
+        const todos = parse(filename, data)
+        console.log(`Found ${todos.length} TODOs in ${filename}`)
+
+        todos.forEach(async (todo) => {
+          if (todo.tag === "TODO") {
+            const title = `TODO: ${todo.text}`;
+
+            // Fetch the list of open issues
+            const {
+              data: issues
+            } = await octokit.issues.listForRepo({
+              owner,
+              repo,
+              state: 'open',
+            });
+
+            // Check if an issue with the same title already exists
+            const existingIssue = issues.find(issue => issue.title === title);
+
+            if (!existingIssue) {
+              console.log(`Creating issue for TODO: ${todo.text}`);
+              octokit.issues.create({
+                owner,
+                repo,
+                title,
+                body: `This issue is automatically created by a build script.\n\nFile: ${todo.file}\nLine: ${todo.line}\nDescription: ${todo.text}`,
+              });
+            } else {
+              console.log(`Skipping issue for TODO: ${todo.text} as it already exists.`);
+            }
+          }
+        })
+      })
+    }
+  }
+}
+
+fromDir(process.cwd(), '.js', function (filename) {
+  console.log('-- found: ', filename);
+})
 
 const date = new Date()
 const formattedDate = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes() < 10 ? "0" : ""}${date.getMinutes()}`
 const commitMessage = `buildbot: ${formattedDate}`
-
 execSync(
-  `git add . && git commit --allow-empty -m "${commitMessage}" && git push`,
-  { stdio: "inherit" },
+  `git add . && git commit --allow-empty -m "${commitMessage}" && git push`, {
+    stdio: "inherit"
+  },
 )
---
+++ frontend/server/api/auth/[...].ts
@@ -0,0 +1,93 @@
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { NuxtAuthHandler } from '#auth'
+
+export default NuxtAuthHandler({
+  /* pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error',
+    verifyRequest: '/auth/verify-request',
+    newUser: '/auth/new-user'
+  }, */
+  // TODO: Build custom auth pages - /auth/signin, /auth/signout, /auth/error, /auth/verify-request, /auth/new-user
+  // TODO: Configure .env variables for the secret and the providers
+  secret: process.env.AUTH_SECRET || 'my-auth-secret',
+
+  providers: [
+    CredentialsProvider.default({
+      name: 'Credentials',
+      
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      authorize (credentials: any) {
+        console.warn('ATTENTION: You should replace this with your real providers or credential provider logic! The current setup is not safe')
+        // TODO: Connect this to MongoDB - first check if the user exists, then check if the password is correct.
+        // TODO: Remove testUser
+
+        const testUser = { 
+          _id: '1',
+          email: 'test@email.com',
+          displayName: 'Test User',
+          password: 'password',
+          picture: 'https://www.gravatar.com/avatar/',
+          shortBio: 'This is a test user',
+          longBio: 'This is a test user',
+          role: 'unverified-user',
+          displayEmail: 'test@email.com',
+          website: 'https://example.com',
+        }
+
+        let user = testUser
+
+        if (credentials?.username === user.email && credentials?.password === user.password) {
+          let u = {
+            id: user._id,
+            name: user.displayName,
+            username: user.email,
+            role: user.role,
+            shortBio: user.shortBio,
+            picture: user.picture,
+          }
+          return u
+        } else {
+          console.error('Warning: Malicious login attempt registered, bad credentials provided')
+
+          throw new Error('Invalid credentials')
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+  callbacks: {
+    jwt: async ({
+        token,
+        user
+    }) => {
+        const isSignIn = user ? true : false;
+        if (isSignIn) {
+            token.id = user ? user.id || '' : '';
+            token.role = user ? user.role || '' : '';
+            token.shortBio = user ? user.shortBio || '' : '';
+            //shortBio is saved in the token so it can be accessed without a database query
+            token.picture = user ? user.picture || '' : '';
+
+        }
+        return Promise.resolve(token);
+    },
+    session: async ({
+        session,
+        token
+    }) => {
+        (session as any).user.id = token.id;
+        (session as any).user.role = token.role;
+        (session as any).user.shortBio = token.shortBio;
+        (session as any).user.picture = token.picture;
+
+        return Promise.resolve(session)
+    },
+},
+  
+})

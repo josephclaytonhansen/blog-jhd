938b8a2aa35ac21872644481a2943a808ceeaa54
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-19 10:53:46 -0500

    buildbot: 4/19 10:53

--- backend/commit.js
+++ backend/commit.js
@@ -1,4 +1,7 @@
 import { execSync } from "child_process"
+import { Octokit } from "@octokit/rest"
+import { parse } from "leasot"
+import fs from "fs"
 
 const date = new Date()
 const formattedDate = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes() < 10 ? "0" : ""}${date.getMinutes()}`
@@ -8,3 +11,39 @@ execSync(
   `git add . && git commit --allow-empty -m "${commitMessage}" && git push`,
   { stdio: "inherit" },
 )
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN, // Make sure to set this environment variable
+})
+
+const owner = "josephclaytonhansen";
+const repo = "blog-jhd";
+
+glob("**/*.js", (err, files) => {
+  if (err) {
+    console.error(err);
+    return
+  }
+
+  files.forEach((file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      const todos = parse(data)
+
+      todos.forEach((todo) => {
+        if (todo.tag === "TODO") {
+          octokit.issues.create({
+            owner,
+            repo,
+            title: `TODO: ${todo.text}`,
+            body: `This issue is automatically created by a build script.\n\nFile: ${todo.file}\nLine: ${todo.line}\nDescription: ${todo.text}`,
+          })
+        }
+      })
+    })
+  })
+})

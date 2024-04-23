9ae8316b0fa6a4e0ea49b1ea675c069a7ea6e0f7
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-23 11:37:34 -0500

    buildbot: 4/23 11:37

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,6 +4,32 @@ const email = ref('')
 const displayName = ref('')
 const password = ref('')
 const confirmPassword = ref('')
+const id = ref('')
+
+const anonymizeUser = async (userId) => {
+  let baseUrl = 'http://localhost:3720/user/anonymize'
+  let method = 'PUT'
+  let urlWithId = baseUrl + '/' + userId
+  try {
+    const response = await fetch(urlWithId, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      console.error(`Server responded with status code ${response.status}`)
+    }
+
+    const responseData = await response.json()
+
+    console.log(responseData)
+  } catch (error) {
+    console.error('An error occurred:', error.error? error.error : error.message? error.message : error)
+  }
+
+}
 
 const register = async () => {
   try {
@@ -39,12 +65,18 @@ const register = async () => {
         <p :v-model="data">Data: {{ data }}</p>
         <nuxt-link v-if="status==='unauthenticated'" @click="signIn">Sign in</nuxt-link>
         <nuxt-link v-if="status==='authenticated'" @click="signOut">Sign out</nuxt-link>
+        <hr/>
         <form @submit.prevent="register">
           <input type="text" v-model="email" placeholder="Email" />
           <input type="text" v-model="displayName" placeholder="Display Name" />
           <input type="password" v-model="password" placeholder="Password" />
           <input type="password" v-model="confirmPassword" placeholder="Confirm Password" />
           <button type = "submit">Register</button>
+        </form>
+        <hr/>
+        <form @submit.prevent="anonymizeUser(id)">
+          <input type="text" v-model="id" placeholder="ID" />
+          <button type = "submit">Anonymize User</button>
         </form>
     </div>
 </template>
--- frontend/server/api/auth/[...].ts
+++ frontend/server/api/auth/[...].ts
@@ -1,6 +1,7 @@
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { NuxtAuthHandler } from '#auth'
-
+import axios from 'axios'
+import bcrypt from 'bcryptjs'
 export default NuxtAuthHandler({
   /* pages: {
     signIn: '/auth/signin',
@@ -21,44 +22,40 @@ export default NuxtAuthHandler({
         username: { label: 'Username', type: 'text' },
         password: { label: 'Password', type: 'password' }
       },
-      authorize (credentials: any) {
+      authorize: async (credentials: any) => {
         console.warn('ATTENTION: You should replace this with your real providers or credential provider logic! The current setup is not safe')
-// TODO: Connect this to MongoDB through blogapi.josephhansen.dev - first check if the user exists, then check if the password is correct.
-// TODO: Remove testUser
-
-        const testUser = { 
-          _id: '1',
-          email: 'test@email.com',
-          displayName: 'Test User',
-          password: 'password',
-          picture: 'https://www.gravatar.com/avatar/',
-          shortBio: 'This is a test user',
-          longBio: 'This is a test user',
-          role: 'unverified-user',
-          displayEmail: 'test@email.com',
-          website: 'https://example.com',
+        let usersUrl = 'http://localhost:3720/user'
+        try {
+            const response = await axios.get(usersUrl).then((res) => res).catch((err) => console.error(err))
+            let users = response.data
+            if (users) {
+                let user = users.find((u: any) => u.email === credentials?.username)
+                console.log('User:', user)
+                if (credentials?.username === user.email) {
+                    let passwordMatch = await bcrypt.compare(credentials.password, user.password)
+                    console.log('Password match:', passwordMatch)
+                    if (!passwordMatch) {
+                        console.error('Warning: password match failed')
+                        throw new Error('Invalid credentials')
+                    } else {
+                        let u = {
+                            id: user._id,
+                            name: user.displayName,
+                            username: user.email,
+                            shortBio: user.shortBio,
+                            picture: user.picture,
+                        }
+                        return u
+                    }
+                } else {
+                    console.error('Warning: Malicious login attempt registered, bad credentials provided')
+                    throw new Error('Invalid credentials')
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching users:', error)
         }
-
-        let user = testUser
-
-        if (credentials?.username === user.email && credentials?.password === user.password) {
-          let u = {
-            id: user._id,
-            name: user.displayName,
-            username: user.email,
-            role: user.role,
-            shortBio: user.shortBio,
-            picture: user.picture,
-          }
-          return u
-        } else {
-          console.error('Warning: Malicious login attempt registered, bad credentials provided')
-
-          throw new Error('Invalid credentials')
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
+    }
     })
   ],
   callbacks: {
@@ -69,9 +66,7 @@ export default NuxtAuthHandler({
         const isSignIn = user ? true : false;
         if (isSignIn) {
             token.id = user ? user.id || '' : '';
-            token.role = user ? user.role || '' : '';
             token.shortBio = user ? user.shortBio || '' : '';
-            //shortBio is saved in the token so it can be accessed without a database query
             token.picture = user ? user.picture || '' : '';
 
         }
@@ -82,7 +77,6 @@ export default NuxtAuthHandler({
         token
     }) => {
         (session as any).user.id = token.id;
-        (session as any).user.role = token.role;
         (session as any).user.shortBio = token.shortBio;
         (session as any).user.picture = token.picture;
 

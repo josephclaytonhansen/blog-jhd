c93340ea07a90cfe58b0e84643ab23704e8d135f
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-26 15:56:15 -0500

    buildbot: 4/26 15:56

---
+++ frontend/src/pages/CMS.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    CMS
+</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,12 @@ import axios from 'axios'
 
 const routes = [
     { path: '/', component: () => import('./pages/Home.vue') },  
+    { path: '/login', component: () => import('./pages/Login.vue') },
+    { path: '/register', component: () => import('./pages/Register.vue') },
+    { path: '/cms', component: () => import('./pages/CMS.vue') },
+    { path: '/profile', component: () => import('./pages/Profile.vue') },
+    //add routes here
+    { path: '/:pathMatch(.*)*', component: () => import('./pages/NotFound.vue') }
 ]
 
 const router = createRouter({
@@ -16,56 +22,54 @@ const lockedRoutes = [
 ]
 
 router.beforeEach(async (to, from, next) => {
-  const lockedRoute = lockedRoutes.find(lockedRoute => to.path.startsWith(lockedRoute.path))
-  if (!lockedRoute) {
-    next()
-    return
-  }
-
-  let user = localStorage.getItem('user')
-  let token = localStorage.getItem('token')
-  if (token && user) {
-    let config = {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    };
-    let url = 'http://localhost:3720/user/verify'
-    // TODO: update to production URL
-    let params = {
-      token: token,
-      user: user
-    };
-    try {
-      let response = await axios.post(url, params, config)
-      if (response.status !== 200) {
-        localStorage.clear()
-        next(lockedRoute.redirect)
-        return
-      }
-
-      if (lockedRoute.role === 'admin') {
-        url = 'http://localhost:3720/user/isadmin'
-        response = await axios.post(url, params, config)
+    const lockedRoute = lockedRoutes.find(lockedRoute => to.path.startsWith(lockedRoute.path))
+    if (!lockedRoute) {
+      next()
+      return
+    }
+  
+    let user = localStorage.getItem('user')
+    let token = localStorage.getItem('token')
+    if (token && user) {
+      let config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      };
+      let url = 'http://localhost:3720/user/verify'
+      let params = {
+        token: token,
+        user: user
+      };
+      try {
+        let response = await axios.post(url, params, config)
         if (response.status !== 200) {
           localStorage.clear()
           next(lockedRoute.redirect)
         } else {
-          next()
+          if (lockedRoute.role === 'admin') {
+            url = 'http://localhost:3720/user/isadmin'
+            response = await axios.post(url, params, config)
+            if (response.status !== 200) {
+              localStorage.clear()
+              next(lockedRoute.redirect)
+            } else {
+              next()
+            }
+          } else {
+            next()
+          }
         }
-      } else {
-        next()
+      } catch (err) {
+        localStorage.clear()
+        next(lockedRoute.redirect)
       }
-    } catch (err) {
+    } else {
       localStorage.clear()
       next(lockedRoute.redirect)
     }
-  } else {
-    localStorage.clear()
-    next(lockedRoute.redirect)
-  }
-})
+  })
 
 export default router

0b7856cf117e4db201bc35a7b0dd37336612151a
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-26 19:05:05 -0500

    buildbot: 4/26 19:05

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,12 +1,56 @@
 <script setup>
 import {userStore} from "../userStore"
 import {pinia} from '../main.js'
+import { useToast } from "vue-toastification"
+const toast = useToast()
+import axios from 'axios'
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+const attemptLogin = () => {
+    const store = userStore(pinia)
+    toast.info('Logging in...')
+    let u = username.value
+    let p = password.value
+    let params = {
+        email: u,
+        password: p
+    }
+    let config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+            } 
+        }  
+    let url = 'http://localhost:3720/user/login'
+    try{
+    axios.post(url, params, config)
+    .then((response) => {
+        toast.success('Login successful')
+        if (response.data) {
+            store.login(response.data)
+            setTimeout(() => {
+                window.location.href = '/'
+            }, 2000)
+        }
+    })
+    .catch((error) => {
+        toast.error(error.message || error.error || 'Error logging in')
+    })
+} catch (error) {
+    toast.error(error.message || error.error || 'Error logging in')
+}
+}
+
 </script>
 
 <template>
     <div>
         <h1>Login</h1>
-        <form @submit.prevent="login">
+        <form @submit.prevent="attemptLogin">
             <input type="text" v-model="username" placeholder="Username">
             <input type="password" v-model="password" placeholder="Password">
             <button type="submit">Login</button>

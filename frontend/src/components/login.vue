b9a8914f3475fc6f10e94b982673fe06a3f2a9c1
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-04-26 21:19:03 -0500

    buildbot: 4/26 21:19

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -5,9 +5,14 @@ import { useToast } from "vue-toastification"
 const toast = useToast()
 import axios from 'axios'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const username = ref('')
 const password = ref('')
+const displayName = ref('')
+const isRegistering = ref(false)
 
 const attemptLogin = () => {
     const store = userStore(pinia)
@@ -22,9 +27,9 @@ const attemptLogin = () => {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Accept': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-            } 
-        }  
+            },
+            withCredentials: true
+        }
     let url = 'http://localhost:3720/user/login'
     try{
     axios.post(url, params, config)
@@ -33,7 +38,7 @@ const attemptLogin = () => {
         if (response.data) {
             store.login(response.data)
             setTimeout(() => {
-                window.location.href = '/'
+                router.push('/')
             }, 2000)
         }
     })
@@ -45,15 +50,56 @@ const attemptLogin = () => {
 }
 }
 
+const registerUser = () => {
+    const store = userStore(pinia)
+    toast.info('Registering...')
+    let config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+        },
+        withCredentials: true
+    }
+    let url = 'http://localhost:3720/user/create'
+    axios.post(url, {
+        email: username.value,
+        password: password.value,
+        displayName: displayName.value
+    }, config).then((response) => {
+        toast.success('Your registration was successful. Please check your email for verification.')
+        if (response.data) {
+            store.login(response.data)
+            setTimeout(() => {
+                router.push('/')
+            }, 2000)
+        }
+    }).catch((error) => {
+        toast.error(error.response.data.message || error.message || error.error || 'Error creating user')
+    })
+}
+
+const showRegister = () => {
+    isRegistering.value = true
+}
+
+const showLogin = () => {
+    isRegistering.value = false
+}
+
 </script>
 
 <template>
     <div>
         <h1>Login</h1>
-        <form @submit.prevent="attemptLogin">
-            <input type="text" v-model="username" placeholder="Username">
+        <form @submit.prevent="isRegistering ? registerUser() : attemptLogin()">
+            <input type="text" v-model="username" placeholder="Email">
             <input type="password" v-model="password" placeholder="Password">
-            <button type="submit">Login</button>
+            
+            <input v-if="isRegistering" type="text" v-model="displayName" placeholder="Display Name">
+                
+            <button type="submit">{{ isRegistering ? 'Sign up' : 'Login' }}</button>
+            <p v-if="!isRegistering">Don't have an account? <button @click="showRegister">Sign up</button></p>
+            <p v-if="isRegistering">Already have an account? <button @click="showLogin">Login</button></p>
         </form>
     </div>
 </template>

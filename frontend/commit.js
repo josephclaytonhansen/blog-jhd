ab21bca44a59fb73c5a57c2c301fd25854723515
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-03 15:55:53 -0500

    buildbot: 5/3 15:55

--- frontend/commit.js
+++ frontend/commit.js
@@ -77,7 +77,7 @@ import {
           const todos = parse(filename, data)
   
           for (const todo of todos) {
-            if (todo.indexOf('TODO: ') !== -1) {
+            if (todo) {
               const title = `TODO: ${todo.text}`;
   
               const existingIssue = issues.find(issue => issue.title === title && issue.state === 'open');

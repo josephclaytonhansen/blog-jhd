4eb7189796027a66b96662873f1b8b3197c9251b
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-03 16:03:51 -0500

    buildbot: 5/3 16:03

--- frontend/commit.js
+++ frontend/commit.js
@@ -60,49 +60,41 @@ import {
   console.log(`Current working directory: ${process.cwd()}`)
   
   async function fromDir(startPath, filters, callback) {
-    if (!fs.existsSync(startPath)) {
-      console.log("no dir ", startPath);
-      return;
-    }
     const files = fs.readdirSync(startPath);
-    for (let i = 0; i < files.length; i++) {
-      const filename = path.join(startPath, files[i]);
-      const stat = fs.lstatSync(filename);
-      if (stat.isDirectory()) {
-        await fromDir(filename, filters, callback)
-      } else if (!filename.endsWith('commit.js')) {
-        try {
-          const data = fs.readFileSync(filename, "utf8");
-  
-          const todos = parse(filename, data)
-  
-          for (const todo of todos) {
-            if (todo) {
-              const title = `TODO: ${todo.text}`;
-  
-              const existingIssue = issues.find(issue => issue.title === title && issue.state === 'open');
-  
-              if (!existingIssue && todo.file.indexOf('node_modules') === -1 || todo.file.indexOf('.git') === -1 && todo.file.indexOf('.nuxt') === -1 && todo.file.indexOf('.output') !== -1) {
-                console.log(`Creating issue for TODO: ${todo.text}`);
-                await octokit.issues.create({
-                  owner,
-                  repo,
-                  title,
-                  body: `This issue is automatically created by a build script.\n\nFile: ${todo.file}\nLine: ${todo.line}\nDescription: ${todo.text}`,
-                });
-  
-                await new Promise(resolve => setTimeout(resolve, 1000));
-              } else if (existingIssue) {
-                console.log(`Skipping issue for TODO: ${todo.text} as it already exists.`);
-              }
+    const promises = files.map(async (file) => {
+        const filename = path.join(startPath, file);
+        const stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            await fromDir(filename, filters, callback);
+        } else if (!filename.endsWith('commit.js')) {
+            try {
+                const data = fs.readFileSync(filename, "utf8");
+                const todos = parse(filename, data)
+                for (const todo of todos) {
+                    if (todo) {
+                        const title = `TODO: ${todo.text}`;
+                        const existingIssue = issues.find(issue => issue.title === title && issue.state === 'open');
+                        if (!existingIssue && todo.file.indexOf('node_modules') === -1 || todo.file.indexOf('.git') === -1 && todo.file.indexOf('.nuxt') === -1 && todo.file.indexOf('.output') !== -1) {
+                            console.log(`Creating issue for TODO: ${todo.text}`);
+                            await octokit.issues.create({
+                                owner,
+                                repo,
+                                title,
+                                body: `This issue is automatically created by a build script.\n\nFile: ${todo.file}\nLine: ${todo.line}\nDescription: ${todo.text}`,
+                            });
+                            await new Promise(resolve => setTimeout(resolve, 1000));
+                        } else if (existingIssue) {
+                            console.log(`Skipping issue for TODO: ${todo.text} as it already exists.`);
+                        }
+                    }
+                }
+            } catch (err) {
+                console.error(err)
             }
-          }
-        } catch (err) {
-          console.error(err)
         }
-      }
-    }
-  }
+    });
+    await Promise.all(promises);
+}
   
   await fromDir(path.resolve(process.cwd(), '..'), ['.js', '.vue', '.ts'], function (filename) {
     console.log('-- found: ', filename);

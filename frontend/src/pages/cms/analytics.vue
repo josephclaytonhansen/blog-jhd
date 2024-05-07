85491b20ed661dfca8a8003022cb6e77934b3029
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-07 11:13:37 -0500

    Update analytics.vue

--- frontend/src/pages/cms/analytics.vue
+++ frontend/src/pages/cms/analytics.vue
@@ -1,6 +1,12 @@
 <script setup>
+import { ref } from 'vue'
+const site = ref(process.env.VUE_APP_MATOMO_URL)
 </script>
 
 <template>
-    
+    <div class="w-full h-full overflow-hidden flex flex-row max-h-[100vh]">
+        <div class="w-[100%] h-[100vh] min-w-[300px] min-h-[400px]" style="resize: both; overflow: auto;">
+            <iframe :src="site" style="width: 100%; height: 100%; border: none;"></iframe>
+        </div>
+    </div>
 </template>

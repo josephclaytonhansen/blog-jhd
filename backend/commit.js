0d1a418e23443b3d1dc244344364dbc859134bf0
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-07 12:17:20 -0500

    buildbot: 5/7 12:17

--- backend/commit.js
+++ backend/commit.js
@@ -112,7 +112,7 @@ const date = new Date()
 const formattedDate = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes() < 10 ? "0" : ""}${date.getMinutes()}`
 const commitMessage = `buildbot: ${formattedDate}`
 execSync(
-  `git pull && git add . && git commit --allow-empty -m "${commitMessage}" && git push`, {
+  `git add . && git commit --allow-empty -m "${commitMessage}" && git push`, {
     stdio: "inherit"
   },
 )

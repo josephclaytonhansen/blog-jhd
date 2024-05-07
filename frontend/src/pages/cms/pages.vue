a78b0b318ac0be505e7a7b7ef62c251c263434ca
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-07 10:33:59 -0500

    Update pages.vue

--- frontend/src/pages/cms/pages.vue
+++ frontend/src/pages/cms/pages.vue
@@ -14,7 +14,7 @@
                 The sites parameter is optional and can be used to specify the sites that the page should be created for. If it is not set, the page will be created for all sites. To set specific sites, format the parameter as <code class="p-1 rounded-lg bg-backdrop-1 text-accent-500">'["site1","site2"]'</code>.
             </p>
             <h2 class="text-xl text-text-2 pb-2">Editing a page</h2>
-            <p class="text-text-3 pb-2>
+            <p class="text-text-3 pb-2">
                 To edit a page, navigate to the <code class="p-1 rounded-lg bg-backdrop-1 text-accent-500">frontend/src/pages</code> directory and open the page you want to edit. Unlike posts, pages are meant to be designed, so they don't have a frontend editor. Use Vue components to design the page. Run <code class="p-1 rounded-lg bg-backdrop-1 text-accent-500">npm run buildbot</code> after any changes.
             </p>
             <h2 class="text-xl text-text- pb-2">Deleting a page</h2>

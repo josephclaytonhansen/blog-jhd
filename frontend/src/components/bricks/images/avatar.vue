05185504db996042b53c4dcf725b802c0a47fc0a
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-07 09:47:22 -0500

    fix avatar

--- frontend/src/components/bricks/images/avatar.vue
+++ frontend/src/components/bricks/images/avatar.vue
@@ -24,6 +24,6 @@
 <template>
     <div class="overflow-hidden avatar" :class="classes">
         <img :src="image" alt="avatar" class="w-full h-full object-cover avatar transition-opacity duration-150" :class="!imageLoaded ? 'opacity-0' : 'opacity-100'" @load="onImageLoad"/>
-        <div class = "w-full h-full bg-backdrop-500 flex items-center justify-center avatar transition-opacity duration-150" :class="!imageLoaded ? 'opacity-100' : 'opacity-0'"></div>
+        <div class = "w-full h-full bg-backdrop-500 flex items-center justify-center avatar transition-opacity duration-150 relative -top-[100%]" :class="!imageLoaded ? 'opacity-100' : 'opacity-0'"></div>
     </div>
 </template>
--- frontend/src/pages/cms/users.vue
+++ frontend/src/pages/cms/users.vue
@@ -256,9 +256,9 @@
             <hr class=" border-backdrop-1 border-2 mb-3"/>
             <div class = 'flex flex-row justify-between flex-wrap gap-3'>
                 <div v-for = "user in group.users">
-                    <div class="flex items-center gap-3 max-h-8">
+                    <div class="flex items-center gap-3">
                         <div class="flex items-center gap-1 shrink">
-                            <Avatar :image="user.picture" classes="ring-4 mr-2 ring-accent-500 hover:cursor-pointer hover:ring-accent-300 transition-all duration-300 avatar w-8 h-8" alt="avatar"/>
+                            <Avatar :image="user.picture" classes="ring-2 mr-2 ring-accent-500 hover:cursor-pointer hover:ring-accent-300 hover:ring-4 transition-all duration-300 avatar w-12 h-12" alt="avatar"/>
                             <router-link class=" hover:text-accent-500 transition-all duration-300" :to = "`/profile/${user.displayName.replace(/ /g, '-')}`"><h3>{{user.displayName}}</h3></router-link>
                         </div>
                         <MailCheck v-if = "user.verifiedEmail || user.role === 'user'" class="text-emerald-500"/>

abf006027383a929d767bd299ed65ad7e4fc2755
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-08 13:15:44 -0500

    author box

--- frontend/src/components/bricks/post/authorBox.vue
+++ frontend/src/components/bricks/post/authorBox.vue
@@ -5,6 +5,9 @@
     import { ref, onMounted } from 'vue'
     import Avatar from '@/components/bricks/images/avatar.vue'
 
+    import {useRouter } from 'vue-router'
+    const router = useRouter()
+
     const author = ref({})
     const getAuthor = async(id) => {
         let url = `${process.env.VUE_APP_SERVER_URL}/user/id/` + props.author_id
@@ -40,7 +43,7 @@
         <div class="flex">
             <Avatar :image="author.picture" classes=" mr-2  square-img-container w-12" alt="avatar"/>
             <div class="flex flex-col">
-                <p class='text-md font-semibold'>by {{ author.displayName }}</p>
+                <p class='text-md font-semibold'>by<router-link :to="`/profile/${author.displayName}`" class="hover:text-accent-500 duration-300 transition-all">{{ author.displayName }}</router-link></p>
                 <p class="text-text-2 text-sm italic">{{ author.shortBio }}</p>
             </div>
         </div>
--- frontend/src/components/bricks/post/sidebar.vue
+++ frontend/src/components/bricks/post/sidebar.vue
@@ -3,10 +3,16 @@ import authorBox from './authorBox.vue'
 const props = defineProps({
     author: String
 })
+import {Eye} from 'lucide-vue-next'
 </script>
 
 <template>
     <div class="sidebar grow max-w-[30vw] hidden md:hidden lg:flex flex-col p-5 rounded-xl">
         <authorBox :author_id="props.author" class="hidden lg:block" />
+        <hr class="dividing-line"/>
+        <div class="flex items-center gap-3">
+            <Eye class="mr-2"/>
+            <p class="text-text-2">{{ props.views }}</p>
+        </div>
     </div>
 </template>

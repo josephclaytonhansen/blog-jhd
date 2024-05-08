5bab1886d8a666ae9edfc7fe2bac89ec8e889ca4
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-07 20:31:15 -0500

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,8 @@ const userLoginByEmail = asyncHandler(async (req, res) => {
 })
 
 const checkSessionUser = asyncHandler(async (req, res) => {
-    let user = JSON.parse(req.body.user.user || req.query.user.user)
+    let user = JSON.parse(req.body.user || req.query.user)
+    console.log(user)
     let session = req.body.session || req.query.session
     let userCheck = await User.findOne({ session: { $eq: session } })
     console.log(user._id, userCheck)

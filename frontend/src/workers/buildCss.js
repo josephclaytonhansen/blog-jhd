2ad810e143b2b1fe78a6ccca6857fc1e2ecd01eb
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-08 12:34:55 -0500

    Update buildCss.js

--- frontend/src/workers/buildCss.js
+++ frontend/src/workers/buildCss.js
@@ -41,6 +41,12 @@ ${process.env.ROUND_AVATARS === 'true' ? '.avatar{border-radius:999rem;}\n' : ''
   display: ${process.env.SIDEBAR === 'true' ? 'flex' : 'none!important'};
 }
 
+.sidebar.colorblock_accent{
+  background-color: ${process.env.SIDEBAR_COLOR_BLOCK === 'true' ? 'theme(\'colors.accent.700\')' : 'transparent'};
+  color: ${process.env.SIDEBAR_COLOR_BLOCK === 'true' ? 'theme(\'colors.backdrop.100\')' : 'theme(\'colors.text.0\')'};
+
+}
+
 @layer components {
   #post_content {
     margin: auto;

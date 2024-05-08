dda07eea4da2bcbd33bcbc9861b0110520a9bea6
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-07 20:26:11 -0500

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,7 @@ const userLoginByEmail = asyncHandler(async (req, res) => {
 const checkSessionUser = asyncHandler(async (req, res) => {
     let user = req.body.user || req.query.user
     let session = req.body.session || req.query.session
-    let userCheck = User.findOne({ session: { $eq: session } })
+    let userCheck = await User.findOne({ session: { $eq: session } })
     if (userCheck._id === user._id) {
         if (Date.now() - new Date(userCheck.sessionTimestamp).getTime() < 15 * 60 * 1000) {
             res.status(200)
@@ -53,7 +53,6 @@ const checkSessionUser = asyncHandler(async (req, res) => {
         res.status(401)
         throw new Error('Invalid session')
     }
-
 })
 
 const verifyTokenUser = asyncHandler(async (req, res, next) => {

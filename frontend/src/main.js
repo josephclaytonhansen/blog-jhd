2e6729e84bc2e20b6827f8ab5b86dfac16a951af
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-09 15:25:49 -0500

    table-sort -> CDN

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -17,8 +17,6 @@ export {pinia}
 import {userStore} from './userStore'
 import { createHead } from '@vueuse/head'
 
-import tableSort from 'table-sort-js'
-
 const toastOptions = {
     position: "top-right",
     timeout: 3000,
@@ -47,7 +45,6 @@ import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
 
 app.use(Toast, toastOptions)
-app.provide('tableSort', tableSort)
 app.use(pinia)
 app.use(router)
 app.mount('#app')

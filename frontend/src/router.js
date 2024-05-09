8d060e2262828ecf52a5e8626b8d6c8cafda2b2c
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-09 13:23:36 -0500

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -21,7 +21,6 @@ const routes = [
 { path: '/test', component: () => import('./pages/SinglePost.vue'), props : {id: '663a98e604bbf7593d046b54'} },
 //end blogs
 //tags
-{ path: '/tag/test', component: () => import('./pages/SingleTag.vue'), props : {id: '663ce6e844f7586b42a80345'} },
 //end tags
     { path: '/NotFound', component: () => import('./pages/NotFound.vue')},
     { path: '/:pathMatch(.*)*', component: () => import('./pages/NotFound.vue') }

cdde4e9b4aae2645ae8c6a819e76fbe0c07f53e2
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-09 10:36:36 -0500

    Update tag.vue

--- frontend/src/components/bricks/post/micros/tag.vue
+++ frontend/src/components/bricks/post/micros/tag.vue
@@ -8,7 +8,7 @@ tagD.value = props.tag
 </script>
 
 <template>
-    <div class="colorblock_accent px-1 text-sm rounded-lg italic pointer-cursor w-min">
+    <div class="colorblock_accent px-1 text-sm rounded-lg italic cursor-pointer hover:bg-accent-500 duration-300 transition-all w-min">
         <p>{{tagD}}</p>
     </div>
 </template>

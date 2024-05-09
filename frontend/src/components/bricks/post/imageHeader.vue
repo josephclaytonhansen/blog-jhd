f3e0c63e874e5483eb32ca9b50bd8caa2f889c40
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-09 11:34:02 -0500

    Update imageHeader.vue

--- frontend/src/components/bricks/post/imageHeader.vue
+++ frontend/src/components/bricks/post/imageHeader.vue
@@ -1,24 +1,24 @@
 <script setup>
 import HdImage from '../images/hdImage.vue'
 import { ref } from 'vue'
+const post = ref({})
 const props = defineProps({
-    src: String,
-    title: String,
+    post: Object,
     overlay: {type: Boolean, default: true},
-    author: String,
 })
+post.value = props.post
 </script>
 
 <template>
-    <div class="relative" v-if="overlay===true">
-        <HdImage :image="props.src" :alt="props.title" class="w-full rounded" />
-        <div class="absolute inset-0 bg-black opacity-30"></div>
-        <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl font-header text-white max-w-[80%]">{{props.title}}</h1>
+    <div class="relative rounded" v-if="overlay===true">
+        <HdImage :image="props.post.featuredImage" :alt="props.post.title" class="w-full rounded" />
+        <div class="absolute inset-0 bg-black opacity-30 rounded"></div>
+        <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl font-header text-white max-w-[80%]">{{props.post.title}}</h1>
     </div>
-    <div class="w-full flex flex-col gap-2" v-else>
-        <HdImage :image="props.src" :alt="props.title" class="w-full rounded" />
-        <h1 class="text-4xl font-header">{{props.title}}</h1>
-        <h2 class="text-xs font-header italic">{{props.author}}</h2>
+    <div class="w-full flex flex-col gap-2 rounded" v-else>
+        <HdImage :image="props.post.featuredImage" :alt="props.post.title" class="w-full rounded" />
+        <h1 class="text-4xl font-header">{{props.post.title}}</h1>
+        <h2 class="text-xs font-header italic">{{props.post.author}}</h2>
     </div>
     <hr class="dividing-line"/>
 </template>

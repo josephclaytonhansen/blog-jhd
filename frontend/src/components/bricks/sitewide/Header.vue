c476bd85227293daea19059cdc4cc5dabcdabb11
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-10 11:19:58 -0500

    header

--- frontend/src/components/bricks/sitewide/Header.vue
+++ frontend/src/components/bricks/sitewide/Header.vue
@@ -1,31 +1,24 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const thisPageComponentName = ref('')
+const loadedComponents = ref({})
+const site = window.location.hostname
+
+const { default: componentsPromise } = await import('./HeaderComponents.ts')
+const components = await componentsPromise
+
+for (const [componentName, component] of Object.entries(components)) {
+  loadedComponents.value[componentName] = component
+}
+
+console.log(loadedComponents)
+
+watch(() => props.thisPageComponentName, (newVal) => {
+  thisPageComponentName.value = newVal
+}, { immediate: true })
+</script>
 
 <template>
-  <component :is="loadedComponents[`${site}${thisPageComponentName}`]" v-if="loadedComponents[`${site}${thisPageComponentName}`]"></component>
+  <component :is="loadedComponents[`${site}${thisPageComponentName.value}`]"></component>
 </template>
-    
-    <script>
-import { ref, onMounted, watch, toRef } from 'vue'
-
-export default {
-  props: {
-    thisPageComponentName: String,
-  },
-  async setup(props) {
-  const loadedComponents = ref({});
-  const site = window.location.hostname;
-
-  const { default: componentsPromise } = await import('./HeaderComponents.ts');
-  const components = await componentsPromise;
-
-  for (const [componentName, component] of Object.entries(components)) {
-    loadedComponents.value[componentName] = component;
-  }
-
-  return {
-    loadedComponents,
-    site: ref(site),
-    thisPageComponentName: toRef(props, 'thisPageComponentName'),
-  }
-}
-}
-</script>

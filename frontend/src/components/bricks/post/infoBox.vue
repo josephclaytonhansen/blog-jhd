c9a48a1c498d56703000e7f1ae560dfaf1cbe57e
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-11 13:59:15 -0500

    buildbot: 5/11 13:59

--- frontend/src/components/bricks/post/infoBox.vue
+++ frontend/src/components/bricks/post/infoBox.vue
@@ -14,13 +14,16 @@ import Tags from './micros/tags.vue'
 import {Eye,Calendar, MessageCircleMore} from 'lucide-vue-next'
 import ISOdateStringToRelative from '../../functions/relativeDate.js'
 
+import {useRouter} from 'vue-router'
+const router = useRouter()
+
 </script>
 
 <template>
     <div id = "info-box">
     <hr class="dividing-line"/>
     <div class="flex justify-between items-center text-text-1" v-if="props.sidebar">
-            <h2 @click="router.push(`/category/${category}`)" class="font-header font-bold transition-all duration-300 hover:text-accent-500">{{props.category}} </h2>
+            <h2 @click="router.push(`/category/${props.category}`)" class="font-header font-bold transition-all duration-300 hover:text-accent-500 cursor-pointer">{{props.category}} </h2>
         <hr class="dividing-line-mid shrink" v-if="props.tags.length > 0"/>
         <Tags :tags="props.tags" class='grow' v-if="props.tags.length > 0" />
     </div>

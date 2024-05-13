b6a2520ad4c411aa2246a78dcda13c2848d49e61
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-13 16:34:19 -0500

    buildbot: 5/13 16:34

--- frontend/src/components/bricks/post/sidebar.vue
+++ frontend/src/components/bricks/post/sidebar.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="sidebar grow max-w-[30vw] hidden md:hidden lg:flex flex-col p-5 rounded-xl">
+    <div class="sidebar grow max-w-[30vw] hidden md:hidden lg:flex flex-col p-5 rounded-xl max-h-min">
         <authorBox :author_id="props.post.author" class="hidden lg:block" />
         <infoBox :views="props.post.views" :date="props.post.date" :tags="props.post.tags" :category="props.post.category" :comments="props.post.comments.length" :sidebar="true" />
     </div>

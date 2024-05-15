c13ace5cbf6768d7ce8ca02088ca0df814275b4c
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-15 14:21:28 -0500

    buildbot: 5/15 14:21

---
+++ frontend/src/components/bricks/pagebuilder/pagebuilder.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { ref } from 'vue'
+const rows = ref([0]) // array of row indexes
+
+const addRow = () => {
+    rows.value.push(rows.value.length)
+}
+
+const removeRow = (index) => {
+    rows.value.splice(index, 1)
+}
+
+import Row from './row.vue'
+import {Plus} from 'lucide-vue-next'
+
+</script>
+
+<template>
+    <div class="pagebuilder">
+        <div class="flex flex-col items-center justify-center">
+            <button @click="addRow" class="px-3 py-2 bg-accent-500 text-white rounded hover:bg-accent-600 duration-300 transition-all flex gap-2 mb-2">
+                <Plus/>
+                Add Row
+            </button>
+            <Row v-for="row in rows" :key="row" :row-index="row" @remove-self="removeRow(row)" />
+            <button @click="addRow" class="px-3 py-2 bg-accent-500 text-white rounded hover:bg-accent-600 duration-300 transition-all flex gap-2 mt-2">
+                <Plus/>
+                Add Row
+            </button>
+        </div>
+    </div>
+</template>
--- frontend/src/components/bricks/pagebuilder/row.vue
+++ frontend/src/components/bricks/pagebuilder/row.vue
@@ -1,10 +1,19 @@
 <script setup>
-import { ref,computed } from 'vue'
+import { ref,computed, defineEmits } from 'vue'
 import AddBlock from './addBlock.vue'
+
+import {Trash} from 'lucide-vue-next'
 
 const props = defineProps({
     rowIndex: {type: Number, required: true, default: 0},
 })
+
+const emits = defineEmits(['remove-self'])
+
+const removeSelf = () => {
+    emits('remove-self')
+}
+
 const layout = ref(
     {
         items: 'center',
@@ -27,7 +36,7 @@ const layout = ref(
 
 const rowClasses = computed(() => {
     return [
-        'grid',
+        'grid relative',
         'items-' + layout.value.items,
         'justify-' + layout.value.justify,
         'gap-' + layout.value.gap,
@@ -50,13 +59,22 @@ const blockAdded = (block) => {
     layout.value.columns[block.column].blocks.push(block)
 }
 
+const removeBlock = (block) => {
+    layout.value.columns[block.column].blocks = layout.value.columns[block.column].blocks.filter(b => b.id !== block.id)
+    
+}
+
 </script>
 
 <template>
     <div :class="rowClasses" :style="{ 'grid-template-columns': gridTemplateColumns }">
+        <Trash @click="removeSelf" class="absolute top-0 right-0 p-[1px] bg-accent-500 w-2 h-2 text-white hover:bg-accent-600 duration-150 transition-all" />
         <div  v-for="column in layout.columns" :key="column.index" class="border-2 border-dashed border-accent-500 duration-150 hover:border-accent-200 transition-colors h-full" :class="columnClasses">
-            <div class="blocks w-full h-full">
-                <component v-for="block in column.blocks" :key="block.id" :is="block.type" :block="block" />
+            <div class="blocks w-full h-full relative">
+                <div v-for="block in column.blocks" :key="block.id" class="relative">
+                    <component :is="block.type" :block="block" />
+                    <Trash @click="removeBlock(block)" class="absolute top-0 right-0 p-[1px] bg-accent-500 w-2 h-2 text-white hover:bg-accent-600 duration-150 transition-all" />
+                </div>
                 <AddBlock :column="column" :row="props.rowIndex" @block-added="blockAdded" />
             </div>
         </div>

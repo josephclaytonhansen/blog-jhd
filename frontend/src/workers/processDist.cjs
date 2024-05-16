cf7d23f457262fcf3d567db6151b393f8f9f798b
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-16 12:09:39 -0500

    buildbot: 5/16 12:09

--- frontend/src/workers/processDist.cjs
+++ frontend/src/workers/processDist.cjs
@@ -1,18 +1,17 @@
-const fs = require('fs-extra');
-const path = require('path');
-const { fileURLToPath } = require('url');
+const fs = require('fs-extra')
+const path = require('path')
 
-const dirname = path.dirname(fileURLToPath(require.main.filename));
+const dirname = path.dirname(require.main.filename)
 
-const distDir = path.resolve(dirname, '../../dist');
-const sitePrefix = process.env.SITE_PREFIX;
+const distDir = path.resolve(dirname, '../../dist')
+const sitePrefix = process.env.SITE_PREFIX
 if (!sitePrefix || sitePrefix === '') {
-  throw new Error('SITE_PREFIX environment variable is required');
+  throw new Error('SITE_PREFIX environment variable is required')
 }
-const newDistDir = path.resolve(dirname, `../../dist.${sitePrefix}`);
+const newDistDir = path.resolve(dirname, `../../dist.${sitePrefix}`)
 
 if (fs.existsSync(newDistDir)) {
-  fs.removeSync(newDistDir);
+  fs.removeSync(newDistDir)
 }
 
-fs.copySync(distDir, newDistDir);
+fs.copySync(distDir, newDistDir)

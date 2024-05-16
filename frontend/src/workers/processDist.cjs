3ff57da71ade1b951518611f77f959e3903ebc7d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-16 12:28:22 -0500

    buildbot: 5/16 12:28

--- frontend/src/workers/processDist.cjs
+++ frontend/src/workers/processDist.cjs
@@ -3,12 +3,12 @@ const path = require('path')
 
 const dirname = path.dirname(require.main.filename)
 
-const distDir = path.resolve(dirname, '../dist')
+const distDir = path.resolve(dirname, '../../dist')
 const sitePrefix = process.env.SITE_PREFIX
 if (!sitePrefix || sitePrefix === '') {
   throw new Error('SITE_PREFIX environment variable is required')
 }
-const newDistDir = path.resolve(dirname, `../dist.${sitePrefix}`)
+const newDistDir = path.resolve(dirname, `../../dist.${sitePrefix}`)
 
 if (fs.existsSync(newDistDir)) {
   fs.removeSync(newDistDir)

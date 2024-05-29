2242d4f739028be31c596667d802afeae802675b
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-29 14:15:53 -0500

    buildbot: 5/29 14:15

--- frontend/src/components/bricks/images/avatar.vue
+++ frontend/src/components/bricks/images/avatar.vue
@@ -23,7 +23,7 @@
 
 <template>
     <div class="overflow-hidden avatar aspect-square" :class="classes">
-        <img :src="image" :alt="alt" class="w-full h-full object-cover avatar transition-opacity duration-300" :class="!imageLoaded ? 'opacity-0' : 'opacity-100'" @load="onImageLoad"/>
-        <div class = "w-full h-full bg-backdrop-500 flex items-center justify-center avatar transition-opacity duration-300 relative" :class="!imageLoaded ? 'opacity-100 -top-[100%]' : 'opacity-0'"></div>
+        <img :src="image" :alt="alt" class="rounded-sm w-full h-full object-cover avatar transition-opacity duration-300" :class="!imageLoaded ? 'opacity-0' : 'opacity-100'" @load="onImageLoad"/>
+        <div class = "rounded-sm w-full h-full bg-backdrop-500 flex items-center justify-center avatar transition-opacity duration-300 relative" :class="!imageLoaded ? 'opacity-100 -top-[100%]' : 'opacity-0'"></div>
     </div>
 </template>

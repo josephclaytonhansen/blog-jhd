ee8ccf41393cb5701f4df0d0b3faef0078eaf2d9
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-05-30 15:55:58 -0500

    buildbot: 5/30 15:55

--- frontend/src/components/bricks/post/messageBanner.vue
+++ frontend/src/components/bricks/post/messageBanner.vue
@@ -7,6 +7,10 @@ const props = defineProps({
     linkText: {type: String, default: 'Learn more'}
 })
 
+const navigateToExternalLink = (link) => {
+    window.location.href = link
+}
+
 const bgClass = computed(() => {
     switch (props.type) {
         case 'info':
@@ -26,6 +30,6 @@ const bgClass = computed(() => {
 <template>
     <div class="fixed bottom-0 left-0 w-screen overflow-hidden p-2 flex items-center justify-center gap-5" :class="bgClass">
         <p class="font-body text-sm">{{props.message}}</p>
-        <button v-if="props.link" class="cursor-pointer bg-accent-500 px-4 py-1 rounded-lg text-text-0 hover:bg-accent-600 hover:scale-105 transition-all duration-300" @click="window.location.href=props.link">{{ props.linkText }}</button>
+        <button v-if="props.link" class="cursor-pointer bg-accent-500 px-4 py-1 rounded-lg text-text-0 hover:bg-accent-600 hover:scale-105 transition-all duration-300" @click="navigateToExternalLink(props.link)">{{ props.linkText }}</button>
     </div>
 </template>
